142d573ce8a8325d0836ec92ed8e84168a7d6c54
tatiana <tatianamonteyano27@gmail.com>
2023-08-28 19:33:40 -0300

    itemListContainer actualizacion

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,13 @@
+
+export const ItemCount = () =>{
+    return (
+        <div>
+            <span>-</span>
+            <span>0</span>
+            <span>+</span>
+            <button>Agregar al carrito</button>
+
+
+        </div>
+    );
+};
--- src/components/itemsListContainer.jsx
+++ src/components/itemsListContainer.jsx
@@ -1,6 +1,8 @@
 import Container from 'react-bootstrap/Container'
+import { ItemCount } from './ItemCount'
 export const ItemsListContainer = (props) =>{
     return <Container className="mt-4">
         <h1>{props.greetings}</h1>
+        <ItemCount/>
         </Container>
 }
