ef036bfe4f01d8a53f8ad0c721b6ff56d68f02af
tatiana <tatianamonteyano27@gmail.com>
2023-08-30 22:10:44 -0300

    componentes actualizados y ruteos

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
+import { Prev } from "react-bootstrap/esm/PageItem";
 
+
+const stock = 6;
 export const ItemCount = () =>{
+const [count, setCount] = useState(0);
+    const handleIncreaseCount = () => {
+       if (stock > count) {
+        setCount((Prev) => Prev +1);
+    }
+}
+    const handleDecreaseCount = () => {
+        if (count > 0){
+            setCount((Prev) => Prev -1);
+        }
+      
+    };
+    const onAdd = () =>{
+    alert(count);} 
+    
     return (
-        <div>
-            <span>-</span>
-            <span>0</span>
-            <span>+</span>
-            <button>Agregar al carrito</button>
+     <div>
+        
+        <span onClick={handleDecreaseCount}>-</span>
+        <span>{count}</span>
+        <span onClick={handleIncreaseCount}>+</span>
+        <button onClick={onAdd}> agregar al carrito</button>
 
+    </div>
+);
 
-        </div>
-    );
-};
+}
+
+   
--- src/components/itemsListContainer.jsx
+++ src/components/itemsListContainer.jsx
@@ -1,8 +1,20 @@
+import { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
-import { ItemCount } from './ItemCount'
+import data from "../data/products.json"
+console.log(data)
+
+//import { ItemCount } from './ItemCount'
 export const ItemsListContainer = (props) =>{
+    const [products, setProducts] = useState ([]);
+    useEffect(() => { 
+        const promise = new Promise ((resolve, reject) => {
+            setTimeout (() => resolve (data), 2000);
+        });
+        promise.then(data => setProducts (data));
+    }, []);
+    console.log(products);
     return <Container className="mt-4">
         <h1>{props.greetings}</h1>
-        <ItemCount/>
+        
         </Container>
 }
